9333242dd072b93614fcef9be590411367aae8b8
Wolfkhan66 <cailehwald@googlemail.com>
2016-05-19 14:23:44 +0100

    add list of common stop words, seperate out indexing. save dictionary of search term and number of times it occurs, then save that dictionar
    y in another that saves the url with its word dictionary. then append
    that dictionary to a list so pages can be ranked by their index

--- crawler.py
+++ crawler.py
@@ -2,46 +2,49 @@ import bs4
 import urllib2
 import re
 
-def crawlnext(nexturl):
+
+def crawl_next(next_url):
     try:
-        url = urllib2.urlopen(nexturl).read()
+        url = urllib2.urlopen(next_url).read()
         soup = bs4.BeautifulSoup(url, "html.parser")
 
-        results = soup.body.find_all(string=re.compile('.*{0}.*'.format('the')), recursive=True)
-
-        print 'Found the word "{0}" {1} times\n'.format('the', len(results))
-
-        print(" crawling", nexturl)
+        print(" crawling", next_url)
         for link in soup.findAll('a', attrs={'href': re.compile("^https://www.")}):
             href = link.get('href')
-            title = link.get('title')
             if href not in waiting and href not in crawled:
                 waiting.append(href)
-                # waiting.append(title)
         crawled.append(url)
-        urltext.append(soup.get_text)
-        waiting.remove(nexturl)
+        waiting.remove(next_url)
     except:
-        # print("error with url",  nexturl)
-        waiting.remove(nexturl)
+        # print("error with url",  next_url)
+        waiting.remove(next_url)
+    index_data(next_url, soup)
 
+def index_data(url , soup):
+    results = soup.find_all(string=re.compile('.*{0}.*'.format(search_term)), recursive=True)
+    print 'Found the word "{0}" {1} times\n'.format(search_term, len(results))
 
-def indexdata (crawled_urls, url_text):
-    dfd = 0
+    urldict = {}
+    urldict.update({search_term : len(results)})
+    dict = {}
+    dict.update({url : urldict})
+    index_list.append(dict)
+    print(dict)
+    print(index_list[0])
 
 
 
 waiting = []
 crawled = []
-urltext = []
-seed = 'https://www.google.com'
+index_list = []
+
+seed = 'https://en.wikipedia.org/wiki/Main_Page'
+search_term = 'age'
+max_crawl_length = 30
+
 
 waiting.append(seed)
 
-while len(waiting) > 0 or len(waiting) < 500:
-    crawlnext(waiting[0])
-    indexdata(crawled,urltext)
+while len(waiting) > 0 or len(crawled) < max_crawl_length:
+    crawl_next(waiting[0])
 
-for i in range(0, len(waiting)):
-    print waiting[i]
-    print ("pass" , i)
