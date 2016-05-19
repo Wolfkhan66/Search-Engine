f51dbcfdde9fdb91adff558b269d9db5be4a5310
Wolfkhan66 <cailehwald@googlemail.com>
2016-05-19 12:28:09 +0100

    move to pycharm, search a page for a search_term, count how many times that word occurs

--- crawler.py
+++ crawler.py
@@ -1,33 +1,46 @@
-from bs4 import BeautifulSoup
+import bs4
 import urllib2
 import re
 
-def CrawlNext(nexturl):
+def crawlnext(nexturl):
     try:
         url = urllib2.urlopen(nexturl).read()
-        soup = BeautifulSoup(url, "html.parser")
+        soup = bs4.BeautifulSoup(url, "html.parser")
+
+        results = soup.body.find_all(string=re.compile('.*{0}.*'.format('the')), recursive=True)
+
+        print 'Found the word "{0}" {1} times\n'.format('the', len(results))
+
         print(" crawling", nexturl)
         for link in soup.findAll('a', attrs={'href': re.compile("^https://www.")}):
             href = link.get('href')
+            title = link.get('title')
             if href not in waiting and href not in crawled:
                 waiting.append(href)
-
-        crawled.add(url)
-        urltext.add(soup.get_text)
+                # waiting.append(title)
+        crawled.append(url)
+        urltext.append(soup.get_text)
         waiting.remove(nexturl)
     except:
-        print("error with url",  nexturl)
+        # print("error with url",  nexturl)
         waiting.remove(nexturl)
 
-waiting = []
-crawled = set()
-urltext = set()
 
-url = 'https://www.google.com'
-waiting.append(url)
+def indexdata (crawled_urls, url_text):
+    dfd = 0
+
+
+
+waiting = []
+crawled = []
+urltext = []
+seed = 'https://www.google.com'
+
+waiting.append(seed)
 
 while len(waiting) > 0 or len(waiting) < 500:
-    CrawlNext(waiting[0])
+    crawlnext(waiting[0])
+    indexdata(crawled,urltext)
 
 for i in range(0, len(waiting)):
     print waiting[i]
