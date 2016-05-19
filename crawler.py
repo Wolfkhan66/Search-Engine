8ee64d10fc5006ad53233ec7f32cb9beedcbb040
Wolfkhan66 <cailehwald@googlemail.com>
2016-05-19 23:58:04 +0100

    save data into an sqlite3 database

--- crawler.py
+++ crawler.py
@@ -2,8 +2,23 @@ import bs4
 import urllib2
 import re
 import string
-import pickle
+import sqlite3 as lite
+import sys
 
+con = lite.connect('indexed_urls.db')
+
+with con:
+    cur = con.cursor()
+    cur.execute('SELECT SQLITE_VERSION()')
+    data = cur.fetchone()
+    print "SQLite version: %s" % data
+
+'''
+Pass in a url
+Create a connection
+Find and store all the links on that url
+Pass data to indexer
+'''
 def crawl_next(next_url):
     try:
         url = urllib2.urlopen(next_url).read()
@@ -39,8 +54,33 @@ def index_data(url , soup):
                     url_dict.update({search_term : len(results)})
                     dict.update({url : url_dict})
                     index_list.append(dict)
-                    print(dict)
+                    #print(dict)
 
+                    try:
+                        con = lite.connect('indexed_urls.db')
+
+                        cur = con.cursor()
+
+                        cur.execute("DROP TABLE IF EXISTS URLs")
+                        cur.execute("CREATE TABLE IF NOT EXISTS URLs(Id INT, Url TEXT, Words TEXT, WordCount INT);")
+                        cur.execute("INSERT INTO URLs VALUES (?, ?, ?, ?);", (len(crawled), url, search_term, len(results)))
+                        cur.execute("SELECT * FROM URLs")
+                        print(cur.fetchall())
+
+                        con.commit()
+
+                    except lite.Error, e:
+
+                        if con:
+                            con.rollback()
+
+                        print "Error %s:" % e.args[0]
+                        sys.exit(1)
+
+                    finally:
+
+                        if con:
+                            con.close()
     except:
         print ("error")
 
@@ -58,6 +98,12 @@ max_crawl_length = 10
 with open('Stop_Words.txt', 'r') as f:
     stop_list = f.readlines()
 
+waiting.append(seed)
+
+while len(waiting) > 0 and len(crawled) < max_crawl_length:
+    crawl_next(waiting[0])
+
+
 '''''''''''
 with open('Waiting_list.txt', 'r') as f:
     waiting = f.readlines()
@@ -67,12 +113,6 @@ with open('Crawled_list.txt', 'r') as f:
 
 with open('index_file.txt', 'rb') as f:
     index_list = pickle.load(f)
-'''''''''''
-
-waiting.append(seed)
-
-while len(waiting) > 0 and len(crawled) < max_crawl_length:
-    crawl_next(waiting[0])
 
 
 with open('index_file.txt', 'wb') as f:
@@ -83,4 +123,4 @@ with open('Waiting_list.txt', 'wb') as f:
 
 with open('Crawled_list.txt', 'wb') as f:
     pickle.dump(crawled, f)
-
+'''''''''''
