a41e677d6209baacd2e34b7c4d9c786d476b866f
Wolfkhan66 <cailehwald@googlemail.com>
2016-05-19 18:20:08 +0100

    succesfully , search a page for words and find how many times that words occurs on the page. and then index the result storing the
    a dictionary of urls with a dictionary of words and their count for each
    word.

--- crawler.py
+++ crawler.py
@@ -1,13 +1,13 @@
 import bs4
 import urllib2
 import re
+import string
 
 
 def crawl_next(next_url):
     try:
         url = urllib2.urlopen(next_url).read()
         soup = bs4.BeautifulSoup(url, "html.parser")
-
         print(" crawling", next_url)
         for link in soup.findAll('a', attrs={'href': re.compile("^https://www.")}):
             href = link.get('href')
@@ -20,17 +20,27 @@ def crawl_next(next_url):
         waiting.remove(next_url)
     index_data(next_url, soup)
 
-def index_data(url , soup):
-    results = soup.find_all(string=re.compile('.*{0}.*'.format(search_term)), recursive=True)
-    print 'Found the word "{0}" {1} times\n'.format(search_term, len(results))
 
-    urldict = {}
-    urldict.update({search_term : len(results)})
+def index_data(url , soup):
+    url_dict = {}
     dict = {}
-    dict.update({url : urldict})
-    index_list.append(dict)
-    print(dict)
-    print(index_list[0])
+    stuff = soup.get_text()
+
+    try:
+        for i in stuff.split():
+            i.strip(string.punctuation + string.digits + string.whitespace)
+            if i not in stop_list and i not in url_dict and len(i) > 3:
+                search_term = i
+
+                results = soup.find_all(string=re.compile('.*{0}.*'.format(search_term)), recursive=True)
+                print 'Found the word "{0}" {1} times\n'.format(search_term, len(results))
+
+                url_dict.update({search_term : len(results)})
+                dict.update({url : url_dict})
+                index_list.append(dict)
+                print(dict)
+    except:
+        print ("error")
 
 
 
@@ -38,9 +48,12 @@ waiting = []
 crawled = []
 index_list = []
 
-seed = 'https://en.wikipedia.org/wiki/Main_Page'
+seed = 'https://www.google.com'
 search_term = 'age'
 max_crawl_length = 30
+
+with open('Stop_Words.txt', 'r') as f:
+    stop_list = f.readlines()
 
 
 waiting.append(seed)
