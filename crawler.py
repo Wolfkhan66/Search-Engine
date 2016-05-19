29310742bc2123c65091445189646cdc8fb9e142
Wolfkhan66 <cailehwald@googlemail.com>
2016-05-19 22:10:43 +0100

    Attempt to save lists of crawled urls, waiting urls and list of dictionairies to file ( not opening properly once saved)

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ import bs4
 import urllib2
 import re
 import string
-
+import pickle
 
 def crawl_next(next_url):
     try:
@@ -35,11 +35,12 @@ def index_data(url , soup):
 
                 results = soup.find_all(string=re.compile('.*{0}.*'.format(search_term)), recursive=True)
                 print 'Found the word "{0}" {1} times\n'.format(search_term, len(results))
-                if len(results) >= 10:
+                if len(results) >= 1:
                     url_dict.update({search_term : len(results)})
                     dict.update({url : url_dict})
                     index_list.append(dict)
                     print(dict)
+
     except:
         print ("error")
 
@@ -51,14 +52,35 @@ index_list = []
 
 seed = 'https://www.google.com'
 search_term = 'age'
-max_crawl_length = 30
+max_crawl_length = 10
+
 
 with open('Stop_Words.txt', 'r') as f:
     stop_list = f.readlines()
 
+'''''''''''
+with open('Waiting_list.txt', 'r') as f:
+    waiting = f.readlines()
+
+with open('Crawled_list.txt', 'r') as f:
+    crawled = f.readlines()
+
+with open('index_file.txt', 'rb') as f:
+    index_list = pickle.load(f)
+'''''''''''
 
 waiting.append(seed)
 
-while len(waiting) > 0 or len(crawled) < max_crawl_length:
+while len(waiting) > 0 and len(crawled) < max_crawl_length:
     crawl_next(waiting[0])
+
+
+with open('index_file.txt', 'wb') as f:
+    pickle.dump(index_list, f)
+
+with open('Waiting_list.txt', 'wb') as f:
+    pickle.dump(waiting, f)
+
+with open('Crawled_list.txt', 'wb') as f:
+    pickle.dump(crawled, f)
 
