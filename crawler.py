3b832b02030541e114496a6245c0eeaa7e7adc98
Wolfkhan66 <cailehwald@googlemail.com>
2016-05-22 03:31:36 +0100

    remove unused imports, move methods into gui thread so gui can be updated, remove whole html from database and only add words found more tha
    n once to decrease load on the database and increase index speed. remove
    unused list for console now the gui text can be updated from within the
    crawler , indexer and query methods. fixed lists saving now everything
    is within the gui method. speed up indexing by removing words already
    found from the text being searched

--- crawler.py
+++ crawler.py
@@ -1,190 +1,166 @@
 import bs4
 import urllib2
-import re
 import string
 import sqlite3 as lite
-import sys
 import pickle
 from Tkinter import *
-import operator
 from collections import OrderedDict
-import unicodedata
-
-
-'''
-Pass in a url
-Create a connection
-Find and store all the links on that url
-Pass data to indexer
-'''
-def crawl_next(next_url):
-    try:
-        url = urllib2.urlopen(next_url).read()
-        soup = bs4.BeautifulSoup(url, "html.parser")
-        print(" crawling", next_url)
-        for link in soup.findAll('a', attrs={'href': re.compile("^http")}):
-            href = link.get('href')
-            if href not in waiting and href not in crawled:
-                waiting.append(href)
-        crawled.append(next_url)
-        waiting.remove(next_url)
-        index_data(next_url, soup)
-    except:
-        # print("error with url",  next_url)
-        waiting.remove(next_url)
-
-
-'''
-Pass in a url and soup object
-find and count word occurence in soup object
-save information to a database
-'''
-def index_data(url , soup):
-    url_dict = {}
-    stuff = soup.get_text()
-    for char in string.punctuation:
-        stuff = stuff.replace(char," ")
-
-    try:
-        con = lite.connect('indexed_urls.db')
-        cur = con.cursor()
-        try:
-            for i in stuff.split():
-                # if the word i is not already in the temporary dictionary or in the stop list and is over 3 characters long and less than 35 characters in length
-                # then i will be our search term
-                if i not in url_dict and i not in stop_list and len(i) >= 3 and len(i) <= 35:
-                    search_term = str(i)
-
-                    # count how many times this word appears on the page
-                    word_count = stuff.count(i)
-                    print("found " + i + " " + str(word_count) + " times")
-                    # if the word appears at least once update the temporary dictionary and open the database
-                    if word_count >= 1:
-                        url_dict.update({search_term : word_count})
-                        title = soup.title.string
-
-                        cur.execute(
-                            "CREATE TABLE IF NOT EXISTS URLs(Id INTEGER PRIMARY KEY, UrlNumber INT, Url TEXT, Title TEXT, UrlText TEXT, Words TEXT, WordCount INT);")
-                        cur.execute("INSERT INTO URLs VALUES (NULL, ?, ?, ?, ?, ?, ?);",
-                                    (len(crawled), url, title, stuff, search_term, word_count))
-
-                        con.commit()
-        except:
-            print ("error")
-
-    except lite.Error, e:
-        if con:
-            con.rollback()
-        print "Error %s:" % e.args[0]
-        sys.exit(1)
-    finally:
-        if con:
-            con.close()
-
-
-'''
-save the waiting and crawled list to file
-'''
-def save_crawl_lists(crawled, waiting):
-    with open('Waiting_list.txt', 'wb') as f:
-        pickle.dump(waiting, f)
-
-    with open('Crawled_list.txt', 'wb') as f:
-        pickle.dump(crawled, f)
-
-    with open('Stop_Words.txt', 'wb') as f:
-        pickle.dump(stop_list, f)
-
-
-'''
-query the database for a term
-'''
-def query_database(search_term):
-
-    try:
-        ranking_dict = {}
-        del console_list[:]
-        console_list.append("###############################")
-        console_list.append("###############################")
-        console_list.append("Querying Database for " + search_term)
-
-        for i in search_term.split():
-            con = lite.connect('indexed_urls.db')
-            cur = con.cursor()
-            cur.execute("SELECT Url FROM URLs WHERE Words LIKE ? ORDER BY WordCount DESC LIMIT 8;",('%'+i+'%',))
-            urls = cur.fetchall()
-            cur.execute("SELECT WordCount FROM URLs WHERE Words LIKE ? ORDER BY WordCount DESC LIMIT 8;", ('%'+i+'%',))
-            wordcount = cur.fetchall()
-            cur.execute("SELECT Url FROM URLs WHERE Url LIKE ? ORDER BY WordCount DESC LIMIT 8;", ('%'+i+'%',))
-            urls_with_term = cur.fetchall()
-            cur.execute("SELECT Url FROM URLs WHERE Title LIKE ? ORDER BY WordCount DESC LIMIT 8;", ('%'+i+'%',))
-            titles_with_term = cur.fetchall()
-
-            if len(urls) >= 1:
-                for lines in urls:
-                    print lines
-                    if lines not in ranking_dict:
-                        ranking_dict[lines] = wordcount[urls.index(lines)]
-                    else:
-                        ranking_dict[lines] = ranking_dict[lines] + wordcount[urls.index(lines)]
-
-            if len(urls_with_term) >= 1:
-                for lines in urls_with_term:
-                    if lines not in ranking_dict:
-                        ranking_dict[lines] = (5,)
-                    else:
-                        ranking_dict[lines] = ranking_dict[lines] + (5,)
-
-            if len(titles_with_term) >= 1:
-                for lines in urls_with_term:
-                    if lines not in ranking_dict:
-                        ranking_dict[lines] = (5,)
-                    else:
-                        ranking_dict[lines] = ranking_dict[lines] + (5,)
-
-            else:
-                console_list.append("no results found for " + i)
-                console_list.append("")
-                console_list.append("###############################")
-                console_list.append("###############################")
-
-        for item in ranking_dict:
-            ranking_dict[item] = sum(ranking_dict[item])
-
-        ranking_dict = OrderedDict(sorted(ranking_dict.items(), key=lambda t: t[1], reverse=True))
-
-        for item in ranking_dict:
-            temp = str(item)
-            temp = temp[:-3]
-            temp = temp[+3:]
-            #unicodedata.normalize('NFKD', str(temp)).encode('ascii', 'ignore')
-            temp2 = str(ranking_dict[item])
-            console_list.append("Rank:[ " + temp2 + " ] " + temp)
-            console_list.append("")
-
-    except lite.Error, e:
-        if con:
-            con.rollback()
-
-        print "Error %s:" % e.args[0]
-        sys.exit(1)
-
-    finally:
-        if con:
-            con.close()
-
 
 '''
 initialize the Graphical User Interface
 '''
 def init_gui():
 
+    '''
+    Pass in a url
+    Create a connection
+    Find and store all the links on that url
+    Pass data to indexer
+    '''
+    def crawl_next(next_url):
+        try:
+            url = urllib2.urlopen(next_url).read()
+            soup = bs4.BeautifulSoup(url, "html.parser")
+            print(" crawling", next_url)
+            for link in soup.findAll('a', attrs={'href': re.compile("^http")}):
+                href = link.get('href')
+                if href not in waiting and href not in crawled:
+                    waiting.append(href)
+            crawled.append(next_url)
+            waiting.remove(next_url)
+            search_engine.update()
+            index_data(next_url, soup)
+        except:
+            # print("error with url",  next_url)
+            waiting.remove(next_url)
+
+
+    '''
+    Pass in a url and soup object
+    find and count word occurence in soup object
+    save information to a database
+    '''
+    def index_data(url, soup):
+        url_dict = {}
+        stuff = soup.get_text()
+        for char in string.punctuation:
+            stuff = stuff.replace(char," ")
+
+        try:
+            con = lite.connect('indexed_urls.db')
+            cur = con.cursor()
+            try:
+                for i in stuff.split():
+                    search_engine.update()
+                    # if the word i is not already in the temporary dictionary or in the stop list and is over 3 characters long and less than 35 characters in length
+                    # then i will be our search term
+                    if i not in url_dict and i not in stop_list and len(i) >= 3 and len(i) <= 35:
+                        search_term = str(i)
+
+                        # count how many times this word appears on the page
+                        word_count = stuff.count(i)
+                        stuff = stuff.replace(i ,'')
+                        # if the word appears more than once update the temporary dictionary and open the database
+                        if word_count >= 2:
+                            print("found " + i + " " + str(word_count) + " times")
+                            url_dict.update({search_term : word_count})
+                            title = soup.title.string
+
+                            cur.execute(
+                                "CREATE TABLE IF NOT EXISTS URLs(Id INTEGER PRIMARY KEY, UrlNumber INT, Url TEXT, Title TEXT, Words TEXT, WordCount INT);")
+                            cur.execute("INSERT INTO URLs VALUES (NULL, ?, ?, ?, ?, ?);",
+                                        (len(crawled), url, title, search_term, word_count))
+
+                            con.commit()
+            except:
+                print ("error")
+
+        except lite.Error, e:
+            if con:
+                con.rollback()
+            print "Error %s:" % e.args[0]
+            sys.exit(1)
+        finally:
+            if con:
+                con.close()
+
+
+    '''
+    query the database for a term
+    '''
+    def query_database(search_term):
+
+        try:
+            ranking_dict = {}
+            for i in search_term.split():
+                search_engine.update()
+                con = lite.connect('indexed_urls.db')
+                cur = con.cursor()
+                cur.execute("SELECT Url FROM URLs WHERE Words LIKE ? ORDER BY WordCount DESC LIMIT 6;",('%'+i+'%',))
+                urls = cur.fetchall()
+                cur.execute("SELECT WordCount FROM URLs WHERE Words LIKE ? ORDER BY WordCount DESC LIMIT 6;", ('%'+i+'%',))
+                wordcount = cur.fetchall()
+                cur.execute("SELECT Url FROM URLs WHERE Url LIKE ? ORDER BY WordCount DESC LIMIT 6;", ('%'+i+'%',))
+                urls_with_term = cur.fetchall()
+                cur.execute("SELECT Url FROM URLs WHERE Title LIKE ? ORDER BY WordCount DESC LIMIT 6;", ('%'+i+'%',))
+                titles_with_term = cur.fetchall()
+
+                if len(urls) >= 1:
+                    for lines in urls:
+                        print lines
+                        if lines not in ranking_dict:
+                            ranking_dict[lines] = wordcount[urls.index(lines)]
+                        else:
+                            ranking_dict[lines] = ranking_dict[lines] + wordcount[urls.index(lines)]
+                if len(urls_with_term) >= 1:
+                    for lines in urls_with_term:
+                        if lines not in ranking_dict:
+                            ranking_dict[lines] = (5,)
+                        else:
+                            ranking_dict[lines] = ranking_dict[lines] + (5,)
+                if len(titles_with_term) >= 1:
+                    for lines in urls_with_term:
+                        if lines not in ranking_dict:
+                            ranking_dict[lines] = (5,)
+                        else:
+                            ranking_dict[lines] = ranking_dict[lines] + (5,)
+
+                else:
+                    console.insert(END,"no results found for " + i + "\n")
+                    console.insert(END,"\n")
+
+            for item in ranking_dict:
+                ranking_dict[item] = sum(ranking_dict[item])
+            ranking_dict = OrderedDict(sorted(ranking_dict.items(), key=lambda t: t[1], reverse=True))
+
+            for item in ranking_dict:
+                temp = str(item)
+                temp = temp[:-3]
+                temp = temp[+3:]
+                temp2 = str(ranking_dict[item])
+                console.insert(END,"Rank:[ " + temp2 + " ] " + temp + "\n")
+                console.insert(END,"\n")
+
+        except lite.Error, e:
+            if con:
+                con.rollback()
+
+            print "Error %s:" % e.args[0]
+            sys.exit(1)
+
+        finally:
+            if con:
+                con.close()
+
+
     def start_query(search_term):
-        query_database(search_term)
         console.delete('1.0', END)
-        for i in range(0 , len(console_list)):
-            console.insert(END, console_list[i])
-            console.insert(END, '\n')
+        console.insert(END,"###############################\n")
+        console.insert(END,"###############################\n")
+        console.insert(END,"Querying Database for " + search_term + "\n")
+        search_engine.update()
+        query_database(search_term)
+
 
     def start_crawl(crawl_length):
         console.delete('1.0', END)
@@ -192,13 +168,16 @@ def init_gui():
         search_engine.update()
         count = int(crawl_length)
         while len(waiting) > 0 and count > 0:
-            crawl_next(waiting[0])
-            count -= 1
             console.insert(END, "Crawling ")
             console.insert(END, waiting[0])
             console.insert(END, "\n")
             search_engine.update()
+            crawl_next(waiting[0])
+            count -= 1
         console.insert(END, "Crawl Finished! \n")
+        console.insert(END, str(len(crawled)) + " Websites have been Indexed\n")
+        console.insert(END, str(len(waiting)) + " Websites are waiting to be Crawled\n")
+
 
     search_engine = Tk()
     search_engine.title("Simple Search Engine")
@@ -236,8 +215,26 @@ def init_gui():
     console = Text(app)
     console.pack()
     console.grid()
+    console.insert(END, "Welcome to my simple Search Engine \n")
+    console.insert(END, str(len(crawled)) + " Websites have been Indexed\n")
+    console.insert(END, str(len(waiting)) + " Websites are waiting to be Crawled\n")
+    console.insert(END, "Please be paitent when running a query :) \n")
 
     search_engine.mainloop()
+
+
+'''
+save the waiting and crawled list to file
+'''
+def save_crawl_lists(crawled, waiting):
+    with open('Waiting_list.txt', 'wb') as f:
+        pickle.dump(waiting, f)
+
+    with open('Crawled_list.txt', 'wb') as f:
+        pickle.dump(crawled, f)
+
+    with open('Stop_Words.txt', 'wb') as f:
+        pickle.dump(stop_list, f)
 
 
 '''
@@ -258,8 +255,6 @@ else:
     crawled = []
 
 print(stop_list)
-console_list = []
-console_list.append("Welcome to my simple Search Engine \n")
 init_gui()
 save_crawl_lists(crawled, waiting)
 
